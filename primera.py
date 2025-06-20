2e0a5021d91039d685e1d4e4b0dbb31cac65af0e
Gabo02-zurita <gabrielzurita487@gmail.com>
2025-06-19 23:35:44 -0500

    Update primera.py

--- primera.py
+++ primera.py
@@ -26,7 +26,7 @@ st.markdown(
     background-color: rgba(255, 255, 255, 0.8);
     padding: 20px;
     border-radius: 10px;
-    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+    box-shadow: 0px 4px 8px rgba(255, 255, 255, 0.1);
 }}
 
 .stSidebar {{
