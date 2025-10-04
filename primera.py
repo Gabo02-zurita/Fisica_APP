2cab223aa53f81cabc69383d3cbbddb1b8ab430d
Gabo02-zurita <gabrielzurita487@gmail.com>
2025-10-04 01:35:46 -0500

    Update primera.py

--- primera.py
+++ primera.py
@@ -659,13 +659,14 @@ def plot_caida_plano_impacto_animacion(m_obj, altura_inicial, angulo_plano_deg, 
 st.sidebar.title("Menú de Simulaciones")
 simulation_type = st.sidebar.radio(
      "Selecciona una opción:",
-     "Fundamentos Teóricos",
+     ("Fundamentos Teóricos",
      "Simulación de Colisión 1D",
      "Simulación de Colisión 2D",
      "Cálculo de Impulso y Fuerza Promedio",
      "Péndulo Balístico",
      "Flecha que se Incrusta en un Saco",
      "Caída por Plano Inclinado + Impacto")
+     )
 st.sidebar.markdown("---")
 st.sidebar.info("¡Experimenta con los parámetros para comprender mejor los conceptos físicos!")
 
